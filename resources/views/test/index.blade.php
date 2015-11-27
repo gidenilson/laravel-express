f12c9ce6eed983a5ad8f3005f019a2828f628983
Gidenilson Alves Santiago <gidenilson@gmail.com>
2015-11-27 19:15:02 -0200

    3th commit

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Olá</title>
-    
+    <title>@yield('title')</title>
 
-
-    <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -42,7 +40,7 @@
 </nav>
 
 <div class="container">
-    Olá {{$nome}}!
+    @yield('content')
 </div>
 
 <!-- Scripts -->
